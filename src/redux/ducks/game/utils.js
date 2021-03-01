899e6bc814e24ef371e6f8118902c3d11accecba
DmitriyFriz <dmitriy.bulavko.friz44@gmail.com>
2021-03-01 13:28:49 +0300

    feat: add function transform puzzle into solvable

--- src/redux/ducks/game/utils.js
+++ src/redux/ducks/game/utils.js
@@ -3,13 +3,19 @@ export const getNewPuzzle = (size) => {
 };
 
 const getNewCard = (index, size) => ({
-  top: Math.floor(index / size),
-  left: index % size,
+  top: getRowPosition(index, size),
+  left: getColumnPosition(index, size),
   value: index + 1,
 });
 
+const getRowPosition = (index, size) => Math.floor(index / size);
+const getColumnPosition = (index, size) => index % size;
+
+const getEmptyIndex = (arr) =>
+  arr.findIndex((item, index, currentArr) => item === currentArr.length - 1);
+
 const checkSolved = (arr, size) => {
-  const emptyIndex = arr.findIndex((item, index, currentArr) => item === currentArr.length);
+  const emptyIndex = getEmptyIndex(arr);
 
   const totalSum = arr.reduce((sum, item, index, currentArr) => {
     if (index === emptyIndex) {
@@ -23,9 +29,9 @@ const checkSolved = (arr, size) => {
     return sum + nextSum;
   }, 0);
 
-  const rowNumber = Math.floor(emptyIndex / size) + 1;
+  const emptyRowPosition = getRowPosition(emptyIndex, size);
 
-  return !((totalSum + rowNumber) % 2);
+  return !!((totalSum + emptyRowPosition) % 2);
 };
 
 const getSolved = (arr, size) => {
@@ -33,15 +39,19 @@ const getSolved = (arr, size) => {
     return arr;
   }
 
-  const correctArr = [...arr];
+  const newArr = [...arr];
 
-  // [correctArr[0], correctArr[1]] = [correctArr[1], correctArr[0]]; don't work for first empty
+  const emptyIndex = getEmptyIndex(newArr);
+  const newEmptyIndex = emptyIndex + size < arr.length - 1 ? emptyIndex + size : emptyIndex - size;
 
-  return correctArr;
+  const emptyItem = newArr.splice(emptyIndex, 1)[0];
+  newArr.splice(newEmptyIndex, 0, emptyItem);
+
+  return newArr;
 };
 
-const testArr = [16, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15];
+const testArr = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15];
 
 console.log(checkSolved(testArr, 4));
-console.log(checkSolved(testArr, 4));
-console.log(getSolved(testArr, 4));
+const newTestArr = getSolved(testArr, 4);
+console.log(checkSolved(newTestArr, 4));
